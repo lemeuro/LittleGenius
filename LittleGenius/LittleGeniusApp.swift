91e898ad4755da34e7cd9df98f1694305a000c54
Lem Euro <mr.lemeuro@gmail.com>
2023-03-20 15:50:58 +0000

    Initial Commit

---
+++ LittleGenius/LittleGeniusApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleGeniusApp.swift
+//  LittleGenius
+//
+//  Created by Lem Euro on 20/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct LittleGeniusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
